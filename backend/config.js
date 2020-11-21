fd1b22ec5daf6df4d3894fced27aa8149a70635c
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-11-21 17:19:56 +0600

    Конфиг файл

---
+++ backend/config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const rootPath = __dirname;
+
+module.exports = {
+    rootPath,
+    uploadPath: path.join(rootPath, 'public', 'uploads'),
+    database: 'mongodb://localhost/shopApp',
+    databaseOpt: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+};
